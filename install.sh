543a551fa40f4116ca060b517a2c96b516b895cb
Avery Yen <haplesshero13@gmail.com>
2012-05-04 17:46:05 -0400

    Initial commit of install script and git ignore.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+echo "Cloning git repositories into place:"
+git clone git://github.com/haplesshero13/lc3tools.git
+git clone git://github.com/haplesshero13/lcc-lc3.git
+
+echo
+echo "Now configuring, compiling, and installing lc3tools..."
+cd lc3tools
+./configure
+make install
+
+echo
+echo "Now configuring, compiling, and installing lcc-lc3..."
+cd ../lcc-lc3
+./configure
+make install
+
+echo
+echo "All done. Now you can add $HOME/.lc3 to your PATH if you wish."
