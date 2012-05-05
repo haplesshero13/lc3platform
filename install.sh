3433b0ca3eeb2ebb3d25e08cc92f7f42b324ffdc
Avery Yen <haplesshero13@gmail.com>
2012-05-05 16:49:54 -0400

    Changed to grep to fix issues.

--- install.sh
+++ install.sh
@@ -38,15 +38,15 @@ rm -rf lcc-lc3
 echo "Adding LC3 Platform binaries to PATH environment variable..."
 LC3_PATH="$HOME/\.lc3"
 
-if [ "$PATH" =~ $LC3_PATH ]; then
+if [ `echo "$PATH" | grep "$LC3_PATH"` ]; then
   echo "Path already contains \"$HOME/.lc3\"."
 else
-  if [ "$SHELL" =~ .*zsh ]; then
+  if [ `echo "$SHELL" | grep zsh` ]; then
     echo "# LC3 Tools" >> ~/.zshrc
     echo 'export PATH="$HOME/.lc3:$PATH"' >> ~/.zshrc
     echo "Added $HOME/.lc3 to your PATH in your zshrc."
   fi
-  if [ "$SHELL" =~ .*bash ]; then
+  if [ `echo "$SHELL" | grep bash` ]; then
     echo "# LC3 Tools" >> ~/.bash_profile
     echo 'export PATH="$HOME/.lc3:$PATH"' >> ~/.bash_profile
     echo "Added $HOME/.lc3 to your PATH in your bash_profile."
